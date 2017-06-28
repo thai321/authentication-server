28c90c9777a856488ca4ae5456d2cd6485a4bb26
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-28 16:23:12 -0700

    install jwt-simple, and created token when sign up

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,12 @@
 const User = require('../models/user');
+const jwt = require ('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+  // iat is issue at time
+}
 
 exports.signup = function(req, res, next) {
   // console.log(req.body);
@@ -31,7 +39,7 @@ exports.signup = function(req, res, next) {
 
     // Repond to request indicating the user was created
     // res.json(user);
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 }
