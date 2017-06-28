d6710d992d365c7ebe6141e774fa5ae4b1b5132e
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-28 00:50:27 -0700

    implemented user, database for authentication-server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router')
+const app = express();
+
+const mongoose = require('mongoose');
+//DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
+
+
+// App Setup
+
+// Middlewares of morgan and bodyParser
+app.use(morgan('combined')); // login framework, incomming request, mostly use
+// it for debugging
+
+app.use(bodyParser.json({type: '*/*' }));
+router(app);
+
+//Server Setup
+const port = process.env.PORT || 3000;
+const server = http.createServer(app); // tell http to create a server
+server.listen(port);
+console.log('Server listening on:', port)
